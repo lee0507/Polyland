90ae0cca9f75580170688b75b9b551ca59fcf2a2
lee0507 <2eastbow@gmail.com>
2021-04-27 15:22:34 +0900

    add polylandReportClass

---
+++ PolylandReport/PolylandReport/typeClass.java
@@ -0,0 +1,16 @@
+package PolylandReport;
+
+public class typeClass {
+	static String[] field_name;
+	static String[] field;
+	static int baby_day_cnt;
+	static int child_day_cnt;
+	static int teen_day_cnt;
+	static int adult_day_cnt;
+	static int old_day_cnt;
+	static int baby_night_cnt;
+	static int child_night_cnt;
+	static int teen_night_cnt;
+	static int adult_night_cnt;
+	static int old_night_cnt;
+}
